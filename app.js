ead4e1a12c8075bc0680053d97ff5c1f6f3a51c8
ziterz <ziady.mubaraq@gmail.com>
2022-02-22 09:47:25 +0700

    - add google sign in feature - return access_token on gsignin feature

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/login', (req, res) => {
   res.send('Login success!')
 })
 
+app.get('/gsignin', (req, res) => {
+  let token = "h28b91b2y 81vbu2b192h9iu1h92192"
+  res.status(200).json({ access_token: token });
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
