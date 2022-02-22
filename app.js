d9824945dea96e162f084195b9271efb98c3f5f3
ziterz <ziady.mubaraq@gmail.com>
2022-02-22 09:49:40 +0700

    add get products feature

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ app.get('/login', (req, res) => {
   res.send('Login success!')
 })
 
+app.get('/products', (req, res) => {
+  let data = [
+    {
+      id: 1,
+      name: 'Jacket'
+    },
+    {
+      id: 2,
+      name: 'Sandal'
+    }
+  ]
+  es.status(200).json({ data });
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
